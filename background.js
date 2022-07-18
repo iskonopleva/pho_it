6d046a51e6fe0aa19a5eb9a757ac9f07b7e952fd
iskonopleva <kysfreen@gmail.com>
2022-07-18 16:45:33 -0500

    Initial commit, working version

--- background.js
+++ background.js
@@ -1,36 +1,17 @@
-// NOTIFICATIONS SYNTAX
-/*
-chrome.notifications.create(
-	'test1',
-	{
-		type: 'basic',
-		iconUrl: 'cherry.jpg',
-		title: 'test1',
-		message: 'notifications test',
-		priority: 1
-	}
-)
-*/
 "use strict";
 
-function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = chrome.tabs.query(queryOptions);
-  return tab;
-}
-
 // Once the selection message is received, launch the content script to replace the selection with "Pho"
+// Gotta get the ID of the current tab before executing the content script 
 chrome.runtime.onMessage.addListener( function(message, sender, sendResponse) {
   	if(message.from && message.from === "popup"){
     	switch(message.action){
       		case "START_SELECTION":
-				chrome.tabs.query({currentWindow: true, active : true}, function(tabArray){
-	  				chrome.scripting.executeScript({
+						chrome.tabs.query({currentWindow: true, active : true}, function(tabArray){
+	  					chrome.scripting.executeScript({
 	        			target: { tabId: tabArray[0].id},
 	        			files: ['content_scripts/selector.js']
 	        		}); 
-				});
+						});
         		console.log("Selection replaced");
         		break;
     }
