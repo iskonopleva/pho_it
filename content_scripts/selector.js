86727fbc3f6f8ca83178586edf3ab7b905bea5b0
iskonopleva <kysfreen@gmail.com>
2022-07-20 09:59:59 -0500

    Update

--- content_scripts/selector.js
+++ content_scripts/selector.js
@@ -1,8 +1,9 @@
-function selector() {
+// replaces current selected text in the active tab with the passed argument
+function selector(textReplace) {
   var sel = window.getSelection();
   var range = sel.getRangeAt(0);
   range.deleteContents();
-  range.insertNode(document.createTextNode("Pho"));
+  range.insertNode(document.createTextNode(textReplace));
 }
 
-selector();
+selector("Pho");
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
-// Initialize button with user's preferred color
+// Initialize button with the specified color
 let START = document.getElementById("START");
-START.style.backgroundColor = "#f7d35c";
+START.style.backgroundColor = "#F1AB86";
 
 //  When the button is clicked, send message to background.js to start replacing selections with Pho
 START.addEventListener("click", async () => {
